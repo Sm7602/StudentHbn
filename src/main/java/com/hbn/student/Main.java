a3b7867cfcce9ae96cb39a424cb7e4d7af38c857
Souvik7602 <sm2496444@gmail.com>
2025-10-30 01:49:38 +0530

    update

--- src/main/java/com/hbn/student/Main.java
+++ src/main/java/com/hbn/student/Main.java
@@ -1,9 +1,14 @@
 package com.hbn.student;
 
 import org.hibernate.Session;
-//import org.hibernate.SessionFactory;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+//import org.hibernate.boot.Metadata;
+//import org.hibernate.boot.MetadataSources;
+//import org.hibernate.boot.registry.StandardServiceRegistry;
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.hbn.student.configuration.HibernateConfiguration;
 import com.hbn.student.entity.Student;
 
 public class Main {
@@ -11,9 +16,15 @@ public class Main {
 	public static void main(String[] arg) {
 		Student s=new Student("vikas","sydgy@jfhuauh",23);
 		
-//		Configuration cfg=new Configuration().configure("hibernate.cfg.xml");
-//		SessionFactory sf=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-		Session session=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory().openSession();
+//		StandardServiceRegistry ssr=new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+//		Metadata meta= new MetadataSources(ssr).getMetadataBuilder().build();
+//		SessionFactory sf=meta.buildSessionFactory();
+//		Session session=sf.openSession();
+		 
+//		SessionFactory sf= HibernateConfiguration.getSessionFactory();
+//		Session session=sf.openSession();
+		
+		Session session=HibernateConfiguration.getSessionFactory().openSession();
 		Transaction tx = session.beginTransaction();
 		session.persist(s);
 		tx.commit();
--- src/main/java/com/hbn/student/entity/Student.java
+++ src/main/java/com/hbn/student/entity/Student.java
@@ -1,6 +1,14 @@
 package com.hbn.student.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name="stu")
 public class Student {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String email;
